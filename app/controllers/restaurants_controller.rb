b3d6d1c4dc98d1dd1f5bf4a9c5cea5ccdfdb6283
Pete Burch <peteburch223@gmail>
2016-04-12 16:41:58 +0100

    first pass reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -15,7 +15,7 @@ class RestaurantsController < ApplicationController
 
   def create
     Restaurant.create(restaurant_params)
-    redirect_to '/restaurants'
+    redirect_to restaurants_path
   end
 
   def edit
@@ -25,14 +25,14 @@ class RestaurantsController < ApplicationController
   def update
     @restaurant = Restaurant.find(params[:id])
     @restaurant.update(restaurant_params)
-    redirect_to '/restaurants'
+    redirect_to restaurants_path
   end
 
   def destroy
     @restaurant = Restaurant.find(params[:id])
     @restaurant.destroy
     flash[:notice] = 'Restaurant deleted successfully'
-    redirect_to '/restaurants'
+    redirect_to restaurants_path
   end
 
   def restaurant_params
