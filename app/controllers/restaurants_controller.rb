51a09dd848468c9353e5503c6958db531123eab1
Gabrielle <gabivk@gmail.com>
2016-04-12 13:50:31 +0100

    can add new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,19 @@
 class RestaurantsController < ApplicationController
 
+  def new
+  end
+
   def index
     @restaurants = Restaurant.all
   end
 
+  def create
+    Restaurant.create(restaurant_params)
+    redirect_to '/restaurants'
+  end
+
+  def restaurant_params
+    p params
+    params.require(:restaurant).permit(:name)
+  end
 end
