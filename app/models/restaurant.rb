dfb7f919c799bc8129cf4400f62d933bf343c306
Pete Burch <peteburch223@gmail>
2016-04-15 09:22:56 +0100

    added UML

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,8 @@
 class Restaurant < ActiveRecord::Base
   validates :name, length: {minimum: 3}, uniqueness: true
-  has_many :reviews, dependent: :destroy
   belongs_to :user
   validates :user, presence: true
+  has_many :reviews, dependent: :destroy
   has_many :user_reviews, through: :reviews, source: :user
 
 end
